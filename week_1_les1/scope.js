516294f032695a9dbcf809da084d133afb681d4e
mertckabak <mertkaba@student.arteveldehs.be>
2025-11-19 15:11:02 +0100

    Explained scope

---
+++ week_1_les1/scope.js
@@ -0,0 +1,16 @@
+const message = "Hey daar";
+
+function sayHello() {
+  const message = "Bye!";
+
+  function say() {
+    const newMessage = "YOLO";
+    console.log(message);
+  }
+
+  console.log(newMessage);
+  say();
+}
+
+sayHello();
+console.log(message);
